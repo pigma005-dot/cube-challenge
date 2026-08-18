c64f6ba71958ddd0c1a0e76b1eb4566198e35735
pigma005-dot <pigma005@gmail.com>
2026-08-18 23:59:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 立方体チャレンジ Service Worker
    キャッシュ名はリリースごとに GAME_VER と合わせて更新すること */
-const CACHE = "cube-challenge-v1.27-b20260813.2";
+const CACHE = "cube-challenge-v1.27-b20260818.1";
 const ASSETS = [
   "./",
   "./index.html",
